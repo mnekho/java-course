8ff3b95e45ec0a489e7c7974362ab26fdd60d386
mnekho <mynameisevin@ya.ru>
2019-05-26 00:09:39 +0600

    Added classes

--- sandbox/src/main/java/mn/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/mn/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,21 +6,21 @@ public class MyFirstProgram {
 	  hello("world");
 	  hello("dear user");
 
-	  double len = 11;
-    System.out.println("Площадь квадрата со стороной "+ len + " равна "+ s(len));
+	  Square sq = new Square(6);
+    System.out.println("Площадь квадрата со стороной "+ sq.l + " равна "+ s(sq));
 
-    double a = 5;
-    double b = 7;
-    System.out.println("Площадь прямоугольника со сторонами "+ a + " и " + b + " равна "+ s(a, b));
+    Rectangle r = new Rectangle(5, 7);
+
+    System.out.println("Площадь прямоугольника со сторонами "+ r.a + " и " + r.b + " равна "+ s(r));
   }
 
   public static void hello(String smb) {
     System.out.println("Hello, " + smb + "!");
   }
-  public static double s (double a) {
-    return a *a;
+  public static double s (Square sq) {
+    return sq.l * sq.l;
   }
-  public static double s (double a, double b) {
-    return a *b;
+  public static double s (Rectangle r) {
+    return r.a * r.b;
   }
 }
