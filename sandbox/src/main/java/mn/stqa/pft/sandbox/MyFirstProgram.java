2f0319e82e94d8282906c05354129cd5c61fb1ef
mnekho <mynameisevin@ya.ru>
2019-05-29 00:57:25 +0600

    Lesson changes (methods added)

--- sandbox/src/main/java/mn/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/mn/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,20 +7,18 @@ public class MyFirstProgram {
 	  hello("dear user");
 
 	  Square sq = new Square(6);
-    System.out.println("Площадь квадрата со стороной "+ sq.l + " равна "+ s(sq));
+    System.out.println("Площадь квадрата со стороной "+ sq.l + " равна "+ sq.area());
 
     Rectangle r = new Rectangle(5, 7);
 
-    System.out.println("Площадь прямоугольника со сторонами "+ r.a + " и " + r.b + " равна "+ s(r));
+    System.out.println("Площадь прямоугольника со сторонами "+ r.a + " и " + r.b + " равна "+ r.area());
+
   }
 
   public static void hello(String smb) {
     System.out.println("Hello, " + smb + "!");
   }
-  public static double s (Square sq) {
-    return sq.l * sq.l;
-  }
-  public static double s (Rectangle r) {
-    return r.a * r.b;
+  public static double distance (Point p1, Point p2){
+    return Math.sqrt((Math.pow((p1.x - p2.x), 2))+ Math.pow((p1.y - p2.y), 2));
   }
 }
