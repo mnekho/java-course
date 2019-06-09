f7fb690875e8ccd10c59662beb4b9819e6316872
mnekho <mynameisevin@ya.ru>
2019-06-09 18:30:13 +0600

    Removed unused lines from GroupCreationTests

--- addressbook-web-tests/src/test/java/mn/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/mn/stqa/pft/addressbook/GroupCreationTests.java
@@ -18,11 +18,7 @@ public class GroupCreationTests {
   }
 
   private void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
@@ -47,14 +43,8 @@ public class GroupCreationTests {
   }
 
   private void fillGroupForm(GroupData groupData) {
-    wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).clear();
     wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    wd.findElement(By.name("group_header")).click();
-    wd.findElement(By.name("group_header")).clear();
     wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    wd.findElement(By.name("group_footer")).click();
-    wd.findElement(By.name("group_footer")).clear();
     wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
   }
 
