a5a94937ae36003602007f168e82093dd5673812
mnekho <mynameisevin@ya.ru>
2019-06-09 19:43:44 +0600

    TestBase class added

---
+++ addressbook-web-tests/src/test/java/mn/stqa/pft/addressbook/GroupDeletionTests.java
@@ -0,0 +1,15 @@
+package mn.stqa.pft.addressbook;
+
+import org.testng.annotations.*;
+
+public class GroupDeletionTests extends TestBase {
+
+  @Test
+  public void testGroupDeletion() throws Exception {
+    gotoGroupPage();
+    selectGroup();
+    deleteSelectedGroups();
+    returnToGroupPage();
+  }
+
+}
