ca1e20db705acf6e4be14bae9afeb289e1642d44
mnekho <mynameisevin@ya.ru>
2019-06-22 11:14:23 +0600

    Input optimization

--- addressbook-web-tests/src/test/java/mn/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/mn/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -15,8 +15,13 @@ public class HelperBase {
   }
 
   public void type(By locator, String text) {
-    wd.findElement(locator).clear();
-    wd.findElement(locator).sendKeys(text);
+    if (text != null) {
+      String existingText = wd.findElement(locator).getAttribute("value");
+      if (! text.equals(existingText)) {
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+      }
+    }
   }
 
   public void click(By locator) {
