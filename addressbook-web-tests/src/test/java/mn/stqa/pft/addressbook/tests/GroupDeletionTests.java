e8b6f3edd89803ac9f3c92cac9172b0c264e78f5
mnekho <mynameisevin@ya.ru>
2019-06-22 23:04:06 +0600

    Pre-requisites for group modification and deletion are added

--- addressbook-web-tests/src/test/java/mn/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/mn/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,5 +1,6 @@
 package mn.stqa.pft.addressbook.tests;
 
+import mn.stqa.pft.addressbook.model.GroupData;
 import org.testng.annotations.*;
 
 public class GroupDeletionTests extends TestBase {
@@ -7,6 +8,9 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
