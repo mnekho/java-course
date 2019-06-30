29aa9fb3c78a90120ffab7540ac8a0f6c2ac1585
mnekho <mynameisevin@ya.ru>
2019-07-01 00:18:50 +0600

    One browser window is used for test suite

--- addressbook-web-tests/src/test/java/mn/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/mn/stqa/pft/addressbook/tests/TestBase.java
@@ -2,19 +2,19 @@ package mn.stqa.pft.addressbook.tests;
 
 import mn.stqa.pft.addressbook.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
-  @BeforeMethod(alwaysRun = true)
+  @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
     app.init();
   }
 
-  @AfterMethod(alwaysRun = true)
+  @AfterSuite(alwaysRun = true)
   public void tearDown() throws Exception {
     app.stop();
   }
