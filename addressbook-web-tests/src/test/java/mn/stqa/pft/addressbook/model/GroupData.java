0df3c7cf43c2782d9fab2077aa63c3f9be3e1eb8
mnekho <mynameisevin@ya.ru>
2019-06-30 17:46:27 +0600

    Added comparison of group lists to GroupCreation test

--- addressbook-web-tests/src/test/java/mn/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/mn/stqa/pft/addressbook/model/GroupData.java
@@ -3,27 +3,31 @@ package mn.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-  private final String id;
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
 
   public GroupData(String name, String header, String footer) {
-    this.id = null;
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public GroupData(String id, String name, String header, String footer) {
+  public GroupData(int id, String name, String header, String footer) {
     this.id = id;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public String getId() {
+  public int getId() {
     return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
   }
 
   public String getName() {
@@ -50,13 +54,18 @@ public class GroupData {
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
+
     GroupData groupData = (GroupData) o;
-    return Objects.equals(id, groupData.id) &&
-        Objects.equals(name, groupData.name);
+
+    if (id != groupData.id) return false;
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
+
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, name);
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
   }
 }
