b322c803c6a95f941dff65033a4fdba0bc90ef2d
mnekho <mynameisevin@ya.ru>
2019-07-08 01:06:03 +0600

    Added fluent interfaces for groups

--- addressbook-web-tests/src/test/java/mn/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/mn/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -13,14 +13,14 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
     app.goTo().groupPage();
     List<GroupData> before = app.group().list();
-    GroupData group = new GroupData("test11", null, null);
+    GroupData group = new GroupData().withName("test2");
     app.group().create(group);
     List<GroupData> after = app.group().list();
     Assert.assertEquals(after.size(), before.size() + 1);
 
 
     Comparator<? super GroupData> byId = Comparator.comparingInt(GroupData::getId);
-    group.setId(after.stream().max(byId).get().getId());
+    group.withId(after.stream().max(byId).get().getId());
     before.add(group);
     before.sort(byId);
     after.sort(byId);
