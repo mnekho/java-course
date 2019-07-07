445354f3d6c9cb4aa5eed04d067c844cd3f739ba
mnekho <mynameisevin@ya.ru>
2019-07-08 02:37:39 +0600

    Group tests are modified to use unique group identifiers

--- addressbook-web-tests/src/test/java/mn/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/mn/stqa/pft/addressbook/model/GroupData.java
@@ -27,6 +27,25 @@ public class GroupData {
     return this;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    if (id != groupData.id) return false;
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
+  }
+
   public GroupData withFooter(String footer) {
     this.footer = footer;
     return this;
@@ -52,17 +71,4 @@ public class GroupData {
         '}';
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return id == groupData.id &&
-        Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, name);
-  }
 }
